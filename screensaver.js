7c3e2aca359537633faee3a31d66be06356353c3
Tony <tonyedvalson@gmail.com>
2024-05-31 17:35:08 -0700

    Initial Commit

---
+++ screensaver.js
@@ -0,0 +1,492 @@
+let screensaverTimeout = null;
+let screensaverOn = false;
+const timeout = 10 * 60 * 1000;
+
+window.onmousemove = function (e) {
+  clearTimeout(screensaverTimeout);
+    screensaverTimeout = setTimeout(function () {
+  }, timeout);
+  if (e.altKey && e.ctrlKey && e.shiftKey) {
+    // Debugging, press alt+ctrl+shift while moving the mouse a bit to start screensaver immediately
+    startScreensaver();
+    return;
+  }
+  if (screensaverOn) {
+    stopScreensaver();
+  }
+};
+
+function startScreensaver() {
+  if (screensaverOn) {
+    return;
+  }
+  screensaverOn = true;
+
+  document.getElementById("screensaver-canvas-container").style.display = "block";
+  reset();
+  animate();
+}
+
+function stopScreensaver() { 
+  if (!screensaverOn) {
+    return;
+  }
+  screensaverOn = false;
+  
+  document.getElementById("screensaver-canvas-container").style.display = "none";
+  reset();
+}
+
+// https://github.com/1j01/pipes/blob/master/screensaver.js with light modifications
+//
+// MIT License
+//
+// Copyright (c) 2018 Isaiah Odhner
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+
+var gridBounds = new THREE.Box3(
+  new THREE.Vector3(-10, -10, -10),
+  new THREE.Vector3(10, 10, 10)
+);
+var nodes = {};
+function setAt(position, value) {
+  nodes["(" + position.x + ", " + position.y + ", " + position.z + ")"] = value;
+}
+function getAt(position, value) {
+  return nodes["(" + position.x + ", " + position.y + ", " + position.z + ")"];
+}
+function clearGrid() {
+  nodes = {};
+}
+
+var textures = {};
+var Pipe = function (scene, options) {
+  var self = this;
+  var pipeRadius = 0.2;
+  var ballJointRadius = pipeRadius * 1.5;
+
+  self.currentPosition = randomIntegerVector3WithinBox(gridBounds);
+  self.positions = [self.currentPosition];
+  self.object3d = new THREE.Object3D();
+  scene.add(self.object3d);
+  if (options.texturePath) {
+    self.material = new THREE.MeshLambertMaterial({
+      map: textures[options.texturePath],
+    });
+  } else {
+    var color = randomInteger(0, 0xffffff);
+    var emissive = new THREE.Color(color).multiplyScalar(0.3);
+    self.material = new THREE.MeshPhongMaterial({
+      specular: 0xa9fcff,
+      color: color,
+      emissive: emissive,
+      shininess: 100,
+    });
+  }
+  var makeCylinderBetweenPoints = function (fromPoint, toPoint, material) {
+    var deltaVector = new THREE.Vector3().subVectors(toPoint, fromPoint);
+    var arrow = new THREE.ArrowHelper(
+      deltaVector.clone().normalize(),
+      fromPoint
+    );
+    var geometry = new THREE.CylinderGeometry(
+      pipeRadius,
+      pipeRadius,
+      deltaVector.length(),
+      10,
+      4,
+      true
+    );
+    var mesh = new THREE.Mesh(geometry, material);
+
+    mesh.rotation.setFromQuaternion(arrow.quaternion);
+    mesh.position.addVectors(fromPoint, deltaVector.multiplyScalar(0.5));
+    mesh.updateMatrix();
+
+    self.object3d.add(mesh);
+  };
+  var makeBallJoint = function (position) {
+    var ball = new THREE.Mesh(
+      new THREE.SphereGeometry(ballJointRadius, 8, 8),
+      self.material
+    );
+    ball.position.copy(position);
+    self.object3d.add(ball);
+  };
+  var makeElbowJoint = function (fromPosition, toPosition, tangentVector) {
+    // "elball" (not a proper elbow)
+    var elball = new THREE.Mesh(
+      new THREE.SphereGeometry(pipeRadius, 8, 8),
+      self.material
+    );
+    elball.position.copy(fromPosition);
+    self.object3d.add(elball);
+  };
+
+  setAt(self.currentPosition, self);
+
+  makeBallJoint(self.currentPosition);
+
+  self.update = function () {
+    if (self.positions.length > 1) {
+      var lastPosition = self.positions[self.positions.length - 2];
+      var lastDirectionVector = new THREE.Vector3().subVectors(
+        self.currentPosition,
+        lastPosition
+      );
+    }
+    if (chance(1 / 2) && lastDirectionVector) {
+      var directionVector = lastDirectionVector;
+    } else {
+      var directionVector = new THREE.Vector3();
+      directionVector[chooseFrom("xyz")] += chooseFrom([+1, -1]);
+    }
+    var newPosition = new THREE.Vector3().addVectors(
+      self.currentPosition,
+      directionVector
+    );
+
+    if (!gridBounds.containsPoint(newPosition)) {
+      return;
+    }
+    if (getAt(newPosition)) {
+      return;
+    }
+    setAt(newPosition, self);
+
+    // joint
+    // (initial ball joint is handled elsewhere)
+    if (lastDirectionVector && !lastDirectionVector.equals(directionVector)) {
+      if (chance(options.ballJointChance)) {
+        makeBallJoint(self.currentPosition);
+      } else {
+        makeElbowJoint(self.currentPosition, newPosition, lastDirectionVector);
+      }
+    }
+
+    // pipe
+    makeCylinderBetweenPoints(self.currentPosition, newPosition, self.material);
+
+    // update
+    self.currentPosition = newPosition;
+    self.positions.push(newPosition);
+  };
+};
+
+var JOINTS_ELBOW = "elbow";
+var JOINTS_BALL = "ball";
+var JOINTS_MIXED = "mixed";
+var JOINTS_CYCLE = "cycle";
+
+var jointsCycleArray = [JOINTS_ELBOW, JOINTS_BALL, JOINTS_MIXED];
+var jointsCycleIndex = 0;
+
+var jointTypeSelect = JOINTS_ELBOW //document.getElementById("joint-types");
+
+var pipes = [];
+var options = {
+  multiple: true,
+  texturePath: null,
+  joints: jointTypeSelect.value,
+  interval: [16, 24], // range of seconds between fade-outs... not necessarily anything like how the original works
+};
+
+var canvasContainer = document.getElementById("screensaver-canvas-container");
+
+// 2d canvas for dissolve effect
+var canvas2d = document.getElementById("screensaver-canvas-2d");
+var ctx2d = canvas2d.getContext("2d");
+
+// renderer
+var canvasWebGL = document.getElementById("screensaver-canvas-webgl");
+var renderer = new THREE.WebGLRenderer({
+  alpha: true,
+  antialias: true,
+  canvas: canvasWebGL,
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+// camera
+var camera = new THREE.PerspectiveCamera(
+  45,
+  window.innerWidth / window.innerHeight,
+  1,
+  100000
+);
+
+// controls
+// var controls = new THREE.OrbitControls(camera, renderer.domElement);
+// controls.enabled = false;
+// controls.autoRotate = true;
+
+// scene
+var scene = new THREE.Scene();
+
+// lighting
+var ambientLight = new THREE.AmbientLight(0x111111);
+scene.add(ambientLight);
+
+var directionalLightL = new THREE.DirectionalLight(0xffffff, 0.9);
+directionalLightL.position.set(-1.2, 1.5, 0.5);
+scene.add(directionalLightL);
+
+// dissolve transition effect
+
+var dissolveRects = [];
+var dissolveRectsIndex = -1;
+var dissolveRectsPerRow = 50;
+var dissolveRectsPerColumn = 50;
+var dissolveTransitionSeconds = 2;
+var dissolveTransitionFrames = dissolveTransitionSeconds * 60;
+var dissolveEndCallback;
+
+function dissolve(seconds, endCallback) {
+  // TODO: determine rect sizes better and simplify
+  // (silly approximation of squares of a particular size:)
+  dissolveRectsPerRow = Math.ceil(window.innerWidth / 20);
+  dissolveRectsPerColumn = Math.ceil(window.innerHeight / 20);
+
+  dissolveRects = new Array(dissolveRectsPerRow * dissolveRectsPerColumn)
+    .fill(null)
+    .map(function (_null, index) {
+      return {
+        x: index % dissolveRectsPerRow,
+        y: Math.floor(index / dissolveRectsPerRow),
+      };
+    });
+  shuffleArrayInPlace(dissolveRects);
+  dissolveRectsIndex = 0;
+  dissolveTransitionSeconds = seconds;
+  dissolveTransitionFrames = dissolveTransitionSeconds * 60;
+  dissolveEndCallback = endCallback;
+}
+function finishDissolve() {
+  dissolveEndCallback();
+  dissolveRects = [];
+  dissolveRectsIndex = -1;
+  ctx2d.clearRect(0, 0, canvas2d.width, canvas2d.height);
+}
+
+var clearing = false;
+var clearTID = -1;
+function clear(fast) {
+  clearTimeout(clearTID);
+  clearTID = setTimeout(
+    clear,
+    random(options.interval[0], options.interval[1]) * 1000
+  );
+  if (!clearing) {
+    clearing = true;
+    var fadeOutTime = fast ? 0.2 : 2;
+    dissolve(fadeOutTime, reset);
+  }
+}
+clearTID = setTimeout(
+  clear,
+  random(options.interval[0], options.interval[1]) * 1000
+);
+
+function reset() {
+  // renderer.dispose(); // Fixes memory leak, not sure if it's the right way to do it, but it works
+  renderer.clear();
+  for (var i = 0; i < pipes.length; i++) {
+    scene.remove(pipes[i].object3d);
+    // dispose of the geometry and materials
+    pipes[i].object3d.traverse(function (object) {
+      if (object.geometry) {
+        object.geometry.dispose();
+      }
+      if (object.material) {
+        object.material.dispose();
+      }
+    });
+  }
+  pipes = [];
+  clearGrid();
+  look();
+  clearing = false;
+}
+
+// this function is executed on each animation frame
+function animate() {
+//   controls.update();
+  if (options.texturePath && !textures[options.texturePath]) {
+    var texture = THREE.ImageUtils.loadTexture(options.texturePath);
+    texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+    texture.repeat.set(2, 2);
+    textures[options.texturePath] = texture;
+  }
+  // update
+  for (var i = 0; i < pipes.length; i++) {
+    pipes[i].update(scene);
+  }
+  if (pipes.length === 0) {
+    var jointType = options.joints;
+    if (options.joints === JOINTS_CYCLE) {
+      jointType = jointsCycleArray[jointsCycleIndex++];
+    }
+    var pipeOptions = {
+      ballJointChance:
+        jointType === JOINTS_BALL ? 1 : jointType === JOINTS_MIXED ? 1 / 3 : 0,
+      texturePath: options.texturePath,
+    };
+    if (chance(1 / 20)) {
+      pipeOptions.texturePath = "textures/candycane.png";
+      // TODO: DRY
+      if (!textures[pipeOptions.texturePath]) {
+        var texture = THREE.ImageUtils.loadTexture(pipeOptions.texturePath);
+        texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+        texture.repeat.set(2, 2);
+        textures[pipeOptions.texturePath] = texture;
+      }
+    }
+    // TODO: create new pipes over time?
+    for (var i = 0; i < 1 + options.multiple * (1 + chance(1 / 10)); i++) {
+      pipes.push(new Pipe(scene, pipeOptions));
+    }
+  }
+
+  if (!clearing) {
+    renderer.render(scene, camera);
+  }
+
+  if (
+    canvas2d.width !== window.innerWidth ||
+    canvas2d.height !== window.innerHeight
+  ) {
+    canvas2d.width = window.innerWidth;
+    canvas2d.height = window.innerHeight;
+    // TODO: DRY!
+    // actually: TODO: make the 2d canvas really low resolution, and stretch it with CSS, with pixelated interpolation
+    if (dissolveRectsIndex > -1) {
+      for (var i = 0; i < dissolveRectsIndex; i++) {
+        var rect = dissolveRects[i];
+        // TODO: could precompute rect in screen space, or at least make this clearer with "xIndex"/"yIndex"
+        var rectWidth = innerWidth / dissolveRectsPerRow;
+        var rectHeight = innerHeight / dissolveRectsPerColumn;
+        ctx2d.fillStyle = "black";
+        ctx2d.fillRect(
+          Math.floor(rect.x * rectWidth),
+          Math.floor(rect.y * rectHeight),
+          Math.ceil(rectWidth),
+          Math.ceil(rectHeight)
+        );
+      }
+    }
+  }
+  if (dissolveRectsIndex > -1) {
+    // TODO: calibrate based on time transition is actually taking
+    var rectsAtATime = Math.floor(
+      dissolveRects.length / dissolveTransitionFrames
+    );
+    for (
+      var i = 0;
+      i < rectsAtATime && dissolveRectsIndex < dissolveRects.length;
+      i++
+    ) {
+      var rect = dissolveRects[dissolveRectsIndex];
+      // TODO: could precompute rect in screen space, or at least make this clearer with "xIndex"/"yIndex"
+      var rectWidth = innerWidth / dissolveRectsPerRow;
+      var rectHeight = innerHeight / dissolveRectsPerColumn;
+      ctx2d.fillStyle = "black";
+      ctx2d.fillRect(
+        Math.floor(rect.x * rectWidth),
+        Math.floor(rect.y * rectHeight),
+        Math.ceil(rectWidth),
+        Math.ceil(rectHeight)
+      );
+      dissolveRectsIndex += 1;
+    }
+    if (dissolveRectsIndex === dissolveRects.length) {
+      finishDissolve();
+    }
+  }
+
+  if (screensaverOn) {
+    requestAnimationFrame(animate);
+  }
+}
+
+function look() {
+  // TODO: never don't change the view (except maybe while clearing)
+  if (chance(1 / 2)) {
+    // head-on view
+
+    camera.position.set(0, 0, 14);
+  } else {
+    // random view
+
+    var vector = new THREE.Vector3(14, 0, 0);
+
+    var axis = new THREE.Vector3(random(-1, 1), random(-1, 1), random(-1, 1));
+    var angle = Math.PI / 2;
+    var matrix = new THREE.Matrix4().makeRotationAxis(axis, angle);
+
+    vector.applyMatrix4(matrix);
+    camera.position.copy(vector);
+  }
+  var center = new THREE.Vector3(0, 0, 0);
+  camera.lookAt(center);
+  // camera.updateProjectionMatrix(); // maybe?
+//   controls.update();
+}
+look();
+
+addEventListener(
+  "resize",
+  function () {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+  },
+  false
+);
+
+/**************\
+  |boring helpers|
+  \**************/
+function random(x1, x2) {
+  return Math.random() * (x2 - x1) + x1;
+}
+function randomInteger(x1, x2) {
+  return Math.round(random(x1, x2));
+}
+function chance(value) {
+  return Math.random() < value;
+}
+function chooseFrom(values) {
+  return values[Math.floor(Math.random() * values.length)];
+}
+function shuffleArrayInPlace(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}
+function randomIntegerVector3WithinBox(box) {
+  return new THREE.Vector3(
+    randomInteger(box.min.x, box.max.x),
+    randomInteger(box.min.y, box.max.y),
+    randomInteger(box.min.z, box.max.z)
+  );
+}
